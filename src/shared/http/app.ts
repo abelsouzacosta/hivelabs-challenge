fee3d6e834783c7126eb1e01a668bd350273bbf3
Abel Souza <abelsouzacosta@gmail.com>
2021-08-06 03:33:36 -0300

    Adicionando middleware de tratamento de erros

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 import 'express-async-errors';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import router from './routes';
 import '@shared/typeorm'; // conexão com o banco de dados
+import ApplicationError from '@errors/ApplicationError';
 
 const app = express();
 
@@ -11,5 +12,23 @@ app.use(cors());
 app.use(express.json());
 
 app.use(router);
+
+// middleware de tratamento de erros
+// middleware usado par o tratametno de erros assíncronos
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof ApplicationError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+      stack: error.stack,
+    });
+  }
+
+  return response.status(400).json({
+    status: 'error',
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 export default app;
