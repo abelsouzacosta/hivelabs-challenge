4f7547b3778675feef638bc748927fd8ab369ddf
Abel Souza <abelsouzacosta@gmail.com>
2021-08-06 03:26:44 -0300

    Adicionando arquivo de rotas da aplicação

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -2,10 +2,13 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import router from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
 
 export default app;
