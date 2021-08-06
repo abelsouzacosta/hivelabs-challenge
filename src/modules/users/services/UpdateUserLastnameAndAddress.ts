dc8824ce2532064c356174cd73175671a3916f34
Abel Souza <abelsouzacosta@gmail.com>
2021-08-06 14:27:31 -0300

    Adicionando UpdateUserLastnameAndAddress
    Serviço que atualiza o sobrenome e o endereço de um usuário

---
+++ src/modules/users/services/UpdateUserLastnameAndAddress.ts
@@ -0,0 +1,60 @@
+import ApplicationError from '@errors/ApplicationError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import testValidUUID from '../providers/TestValidUUID';
+import User from '../typeorm/entities/Users';
+
+interface IUserLastnameAndAddressUpdate {
+  id: string;
+  lastName: string;
+  address: string;
+}
+
+export default class UpdateUserLastnameAndAddress {
+  private repository: UsersRepository;
+
+  constructor() {
+    this.repository = getCustomRepository(UsersRepository);
+  }
+
+  /**
+   * Verifica se um usuário com o endereço passado já existe
+   * @param id string - id do usuário
+   * @returns
+   */
+  private async findUser(id: string): Promise<User | undefined> {
+    const user = await this.repository.findById(id);
+
+    return user;
+  }
+
+  // verifica se o uuid passado é valido
+  private isIdAnUUID(id: string): boolean {
+    return testValidUUID(id);
+  }
+
+  public async execute({
+    id,
+    lastName,
+    address,
+  }: IUserLastnameAndAddressUpdate): Promise<User> {
+    if (!this.isIdAnUUID(id))
+      throw new ApplicationError(
+        'Falied on parameter pattern: an uuid must be provided',
+      );
+
+    const user = await this.findUser(id);
+
+    if (!user) throw new ApplicationError('User not found');
+
+    user.lastName = lastName;
+    user.address = address;
+
+    if (!this.repository.save(user))
+      throw new ApplicationError(
+        "There's an unknown error trying to save user's instance",
+      );
+
+    return user;
+  }
+}
